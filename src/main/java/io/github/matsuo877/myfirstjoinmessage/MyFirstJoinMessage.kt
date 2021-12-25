15c345ec46d23905dd7016d1da16d2dfd0a1310d
matsuo877 <matsuo877@gmail.com>
2021-12-25 14:57:21 +0900

    ほぼ自力で製作

--- src/main/java/io/github/matsuo877/myfirstjoinmessage/MyFirstJoinMessage.kt
+++ src/main/java/io/github/matsuo877/myfirstjoinmessage/MyFirstJoinMessage.kt
@@ -3,34 +3,32 @@ package io.github.matsuo877.myfirstjoinmessage
 import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.plugin.java.JavaPlugin
 
-class MyFirstJoinMessage : JavaPlugin(), Listener {
+class MyFirstJoinMessage : JavaPlugin() , Listener{
     override fun onEnable() {
-        //処理を書く
-        server.pluginManager.registerEvents(this, this)
+        server.pluginManager.registerEvents(this , this)
         saveDefaultConfig()
+        logger.info("Enabled!")
     }
 
     override fun onDisable() {
-        //処理を書く
+        logger.info("Disabled!")
     }
-
     @EventHandler
-    fun onLoginEvent(event: PlayerJoinEvent) {
-        var Login = config.getString("JoinMessage")!!
-        Login = Login.replace("%pl%", event.player.name)
-        Login = Login.replace("%n%".toRegex(), "\n")
-        event.joinMessage = ChatColor.translateAlternateColorCodes('&', Login)
+    fun onLoginEvent(event: PlayerJoinEvent){
+        var login = config.getString("JoinMessage")!!
+        login = login.replace("%pl%" , event.player.name)
+        login = login.replace("%n%" , "\n")
+        event.joinMessage = ChatColor.translateAlternateColorCodes('&' , login)
     }
-
     @EventHandler
-    fun onLogoutEvent(event: PlayerQuitEvent) {
-        var Logout = config.getString("LogoutMessage")!!
-        Logout = Logout.replace("%pl%", event.player.name)
-        Logout = Logout.replace("%n%".toRegex(), "\n")
-        event.quitMessage = ChatColor.translateAlternateColorCodes('&', Logout)
+    fun onLogoutEvent(event: PlayerQuitEvent){
+        var logout = config.getString("LogoutMessage")!!
+        logout = logout.replace("%pl%" , event.player.name)
+        logout = logout.replace("%n%" , "\n")
+        event.quitMessage = ChatColor.translateAlternateColorCodes('&' , logout)
     }
 }
